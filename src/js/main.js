103faa3a4be38e1e4208cb5b925fe40066668bfc
Alex Hilson <alexhilsonwork@googlemail.com>
2016-09-30 14:09:07 +0100

    Update gui endpoints

--- src/js/main.js
+++ src/js/main.js
@@ -94,7 +94,7 @@ function initControls() {
        if(!recording) {
            var recordName = prompt("Please enter a name for this recording");
            request
-               .post(BELL_SERVER+'/record')
+               .post(BELL_SERVER+'/recording')
                .json({record_filename: recordName})
                .on('response', function(response) {
                    alert("Now Recording...");
@@ -103,7 +103,7 @@ function initControls() {
                });
        } else {
            request
-               .get(BELL_SERVER+'/record/stop')
+               .get(BELL_SERVER+'/recording/stop')
                .on('response', function(response) {
                     alert("Recording Stopped");
                     recording = false;
@@ -116,7 +116,7 @@ function initControls() {
     playRecordingButton.addEventListener('click', function(evt) {
         var record = document.getElementById('records-list').value;
         request
-            .get(BELL_SERVER+'/record/'+record.replace('.json', '')+'/play')
+            .get(BELL_SERVER+'/records/'+record.replace('.json', '')+'/play')
     })
 
     listRecordings();
